3f2388604ce867b42b9447f97fb5066df0d77e0c
DACHXY <Danny10132024@gmail.com>
2024-01-28 10:18:24 +0800

    Add TaiwanFactCheck functions

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import asyncio
+from src.websites import TaiwanFactCheck
 
 async def main():
-    print()
+    tfc = TaiwanFactCheck()
+    tfc.get_raw_content()
+    tfc.get_parsed_json()
+    tfc.get_sub_links()
+    print(tfc.sub_links)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- src/websites.py
+++ src/websites.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from Src.handler import ErrorHanlder
+from src.handler import ErrorHanlder
 import re
 
 class GenericWeb:
@@ -11,13 +11,12 @@ class GenericWeb:
         self._raw_content: str = None
         self._json_content: str = None
     
-    @ErrorHanlder.request_error
     def _get_raw_content(self) -> bytes:
         res = self._session.get(self._base_url)
         res.raise_for_status()
         return res.content
 
-    def _parse_json(self, raw_content) -> json:
+    def _parse_json(self) -> json:
         jsonObject = json.loads(self._raw_content)
         self._json_content = jsonObject
         return self._json_content
@@ -30,12 +29,17 @@ class GenericWeb:
     
 class TaiwanFactCheck(GenericWeb):
     def __init__(self):
-        base_url = "https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=10&hl=zh-TW&source=gcsc&gss=.tw&cselibv=3bd4ac03c21554b3&cx=016739345681392223711%3Aurihlbomoaq&q=%E5%BE%B5%E6%89%8D+%E9%8C%AF%E8%AA%A4+%E8%A9%90%E9%A8%99&safe=off&cse_tok=AB-tC_46pyznbk9agghdlTs-qKAB%3A1703828960242&sort=&exp=csqr%2Ccc%2Cdtsq-3&callback=google.search.cse.api1505"
+        
+        # TODO: 解決 google search api 的問題
+        
+        base_url = "https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=20&hl=zh-TW&source=gcsc&gss=.tw&cselibv=8435450f13508ca1&cx=016739345681392223711%3Aurihlbomoaq&q=%E8%A9%90%E9%A8%99%2B%E5%BE%B5&safe=off&cse_tok=AB-tC_7rAJHN0PV3JSKDiJwvzPd1%3A1706406801809&sort=&exp=cc%2Cdtsq-3&fexp=72485384%2C72485385&callback=google.search.cse.api8754"
         web_name = "TaiwanFactCheck"
         super().__init__(base_url, web_name)
+
+        self.sub_links = []
     
     def get_raw_content(self) -> str:
-        self._raw_content = super().get_raw_content()
+        self._raw_content = super()._get_raw_content()
         raw_content = self._raw_content.decode("utf-8").replace("\n", "")
         result = re.search(r"google.search.cse.api\d+\((.*)\)", raw_content)
         if result: 
@@ -43,7 +47,23 @@ class TaiwanFactCheck(GenericWeb):
             return self._raw_content
         raise Exception("Regex Error.")
 
-    def _parse_json(self) -> json:
+    def get_parsed_json(self) -> json:
         self._json_content = super()._parse_json()
         return self._json_content
-    
+
+    def get_sub_links(self):
+        result = self._json_content["results"]
+
+        for i in range(len(result)):
+            try:
+                link = result[i]["url"]
+                title = result[i]["richSnippet"]["metatags"]["ogTitle"]
+                
+                if not (("詐騙" in title or "錯誤" in title) and "徵" in title):
+                    continue
+                if "https://tfc-taiwan.org.tw/articles/" not in link:
+                    continue
+                self.sub_links.append(link)
+            except:
+                pass
+        return self.sub_links
